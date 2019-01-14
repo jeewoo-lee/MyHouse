81f37fe65ee83a651008003b9e8f10836f093d28
Jeewoo Lee <jlee@lcs.on.ca>
2019-01-14 15:18:42 -0500

    made ground, made chumney, changed size of door, size of handle, size of the roof  and adjust the location of the house

--- MyHouse.java
+++ MyHouse.java
@@ -14,6 +14,9 @@ public class MyHouse
     private Square door1;
     private Square door2;
     private Circle handle;
+    private Square ground;
+    private Square chimney1;
+    
 
     /**
      * Constructor for objects of class myHouse
@@ -21,12 +24,15 @@ public class MyHouse
     public MyHouse()
     {
         // initialise instance variables
+        ground = new Square();
         base = new Square();
         roof = new Triangle();
         window = new Circle();
         door1 = new Square();
         door2 = new Square();
         handle = new Circle();
+        chimney1 = new Square();
+        
     }
 
     /**
@@ -36,6 +42,12 @@ public class MyHouse
     public void drawHouse()
     {
         // Arrange each object
+        ground.makeVisible();
+        ground.changeColor("green");
+        ground.changeSize(1000);
+        ground.moveHorizontal(-100);
+        ground.moveVertical(220);
+        
         base.makeVisible();
         base.changeSize(200);
         base.changeColor("Black");
@@ -45,17 +57,24 @@ public class MyHouse
         
         door1.makeVisible();
         door1.changeSize(50);
-        door1.moveHorizontal(55);
-        door1.moveVertical(200);
+        door1.moveHorizontal(60);
+        door1.moveVertical(210);
         door1.moveUp();
         
         door2.makeVisible();
         door2.changeSize(50);
-        door2.moveHorizontal(55);
+        door2.moveHorizontal(60);
         door2.moveVertical(150);
         
         roof.makeVisible();
+        roof.changeColor("red");
+        roof.changeSize(50,250);
         roof.moveHorizontal(100);
+        roof.moveVertical(30);
+        
+        chimney1.makeVisible();
+        chimney1.changeColor("red");
+        chimney1.moveHorizontal(20);
         
         window.makeVisible();
         window.changeSize(50);
@@ -63,8 +82,9 @@ public class MyHouse
         window.moveVertical(60);
         
         handle.makeVisible();
-        handle.moveHorizontal(90);
-        handle.moveVertical(170);
+        handle.changeSize(15);
+        handle.moveHorizontal(100);
+        handle.moveVertical(180);
         handle.changeColor("yellow");
         
         
